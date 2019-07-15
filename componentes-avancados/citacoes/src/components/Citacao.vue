e6d3fb6dbd007a702a6b3b8a4f144c5d41879c6a
Marlon Raphael <marlon.raphaael@hotmail.com>
2019-07-15 07:46:52 -0300

    update pratices

--- componentes-avancados/citacoes/src/components/Citacao.vue
+++ componentes-avancados/citacoes/src/components/Citacao.vue
@@ -1,12 +1,19 @@
 <template>
     <div class="citacao">
-        
+        <slot name="autor"></slot>
+        <slot name="texto"></slot>
+        <slot name="fonte"></slot>
     </div>
 </template>
 
 <script>
 export default {
-
+    activated() {
+        console.log('activated');
+    },
+    deactivated() {
+        console.log('deactivated');
+    }
 }
 </script>
 
---
+++ projeto-02-tarefas/tarefas/src/components/TaskGrid/TaskGrid.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="TaskGrid">
+        <template v-if="tasks.length">
+            <Task v-for="(task, i) in tasks"
+                  @taskDeleted="$emit('taskDeleted', i)"
+                  :key="task.name"
+                  :task="task"
+                  @taskStateChanged="$emit('taskStateChanged', i)">
+            </Task>
+        </template>
+        <p v-else class="no-task">Sua vida está em dia :)</p>
+    </div>
+</template>
+
+<script>
+    import Task from '../Task/Task.vue';
+
+    export default {
+        components: { Task },
+        props: {
+            tasks: { type: Array, required: true}
+        },
+    };
+</script>
+
+<style scoped>
+    .TaskGrid {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
+
+    .TaskGrid .Task {
+        margin: 10px;
+    }
+
+    .no-task {
+        color: #AAA;
+        font-size: 1.7rem;
+    }
+</style>
